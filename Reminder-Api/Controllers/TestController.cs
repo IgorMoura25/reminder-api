8ec9eb6265890d69733aed5674bf3e4396fb0806
Igor Moura <igor.moura254@hotmail.com>
2021-09-04 13:45:19 -0300

    Initial commit

---
+++ Reminder-Api/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Reminder_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Tests = new[]
+        {
+            "Test1", "Test2", "Test3"
+        };
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string[] Get()
+        {
+            return Tests;
+        }
+    }
+}
